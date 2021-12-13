e6c532f7e048710b2af0f7f55220c3026e6c387a
tolufolorunso <tolufolorunso@yahoo.com>
2021-12-13 06:42:07 +0100

    updated

--- app.js
+++ app.js
@@ -85,9 +85,9 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const start = async () => {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   try {
-    await connectDB(process.env.MONGO_URL_LOCAL);
+    await connectDB(process.env.MONGO_URL);
     app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
   } catch (error) {
     console.log(error);
